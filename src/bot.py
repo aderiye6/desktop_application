037e2ed563cefd43b8d5bf5988d17a7649f43370
Fatoki Oladele <fatoki.oladele@gmail.com>
2022-05-05 16:35:23 +0100

    open tab bug fix

--- src/bot.py
+++ src/bot.py
@@ -2,17 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+from time import sleep
 import pandas as pd
 import os
 thisfolder = os.path.dirname(os.path.abspath(__file__))
+
 
 def execScript(script):
   exec(script)
 
 
 class Window:
-  def __init__(self, url=''):
+  def __init__(self, url='https://google.com'):
     self.driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()))
 
@@ -34,8 +35,13 @@ class Window:
   def goToUrl(self, url):
     self.driver.get(url)
 
-  def openTab(self, url=''):
-    self.driver.execute_script(f'window.open({url})')
+  def openTab(self, url='https://google.com'):
+    print('In open tab function')
+    script = f"window.open('{url}','_blank');"
+    print(script)
+    self.driver.execute_script(script)
+    tabs = self.driver.window_handles
+    self.driver.switch_to.window(tabs[-1])
 
   def closeTab(self, tabID):
     tabs = self.driver.window_handles
@@ -48,9 +54,11 @@ class Window:
 
   def goBackHistory(self):
     self.driver.execute_script("window.history.go(-1)")
+    sleep(3)
 
   def goForwardHistory(self):
     self.driver.execute_script("window.history.go(+1)")
+    sleep(3)
 
   def closeBrowser(self):
     self.driver.quit()
@@ -60,7 +68,6 @@ class CSV:
   def __init__(self):
     pass
 
-  def saveDictToCSV(dict, path, filename):
+  def saveDictToCSV(self, dict, path, filename):
     df = pd.DataFrame(dict)
     df.to_csv(os.path.join(path, filename), encoding='utf-8')
-
